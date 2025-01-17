13e33fef31294df565156f744fef1ec142ceb8fe
Ryan Miller <ryan@intranoggin.com>
2025-01-17 15:16:39 -0500

    initial blink commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pi-WS2812b
 WS2812B Light driver for Raspberry Pi 3B
+
+**Pi 3B Pin Layout**
+![Pin Layout](GPIO.png)
---
+++ pi-WS2812B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Device.Gpio;
+using System.Threading;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("VS - Hello, World!");
+
+GpioController controller = new GpioController(PinNumberingScheme.Logical);
+
+var pin = 5;
+var lightTime = 300;
+
+controller.OpenPin(pin, PinMode.Output);
+
+try
+{
+    while (true)
+    {
+        Console.WriteLine("Light on");
+        controller.Write(pin, PinValue.High);
+        Thread.Sleep(lightTime);
+        Console.WriteLine("Light off");
+        controller.Write(pin, PinValue.Low);
+        Thread.Sleep(lightTime);
+    }
+}
+finally
+{
+    controller.ClosePin(pin);
+}
