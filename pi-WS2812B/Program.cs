0e9465048349a97dc07df47d16de8e5822d1dcd0
Ryan Miller <ryan@intranoggin.com>
2025-01-17 17:29:22 -0500

    troubleshooting

--- pi-WS2812B/Program.cs
+++ pi-WS2812B/Program.cs
@@ -8,23 +8,28 @@ SpiConnectionSettings settings = new(0, 0)
 {
     ClockFrequency = 2_400_000,
     Mode = SpiMode.Mode0,
-    DataBitLength = 8
+    DataBitLength = 8    
 };
 var height = 8;
 var width = 8;
 var ledCount = height * width;
+var r = 0;
+var g = 0;
+var b = 0;
 
 using (SpiDevice spi = SpiDevice.Create(settings))
 {
     var ledStrip = new Ws2812b(spi, ledCount);
 
     RawPixelContainer img = ledStrip.Image;
+    
+    Console.WriteLine("Width: " + img.Width);
+    Console.WriteLine("Height: " + img.Height);
     for (var x = 0; x < width; x++)
     {
         for (var y = 0; y < height; y++)
-        {
+        {            
             //var color = new Color();
-
             img.SetPixel(x, y, Color.RebeccaPurple);
         }
 
